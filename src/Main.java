812000c6a1caeceac863c19b3d2c2d766f0947cc
Jang, Kyoung Rok <astray0924@gmail.com>
2012-05-24 22:47:13 +0900

    dsfsdf

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import java.util.List;
 
 import com.joogle.model.YahooAnswer;
 import com.joogle.model.YahooQuestion;
+import com.joogle.utility.URLExtractor;
 
 public class Main {
 
@@ -9,14 +10,31 @@ public class Main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		List<YahooQuestion> questions = YahooAnswerHelper.searchQuestions("beaver");
-		YahooQuestion q = questions.get(2);
-		
-		List<YahooAnswer> answers = YahooAnswerHelper.getAnswers(q);
-		
-		for (YahooAnswer answer : answers) {
-			System.out.println(answer);
+		List<YahooQuestion> questions = YahooAnswerHelper
+				.searchQuestions("organizations group hunger");
+
+//		for (YahooQuestion q : questions) {
+//			System.out.println("[Subject]");
+//			System.out.println(q.Subject);
+//			System.out.println("[Content]");
+//			System.out.println(q.Content);
+//			System.out.println("[Selected Answer]");
+//			System.out.println(q.ChosenAnswer);
+//			System.out
+//					.println("----------------------------------------------------------");
+//		}
+
+		String corpus = "";
+
+		for (YahooQuestion q : questions) {
+			corpus += (" " + q.Subject);
+			corpus += (" " + q.Content);
+			corpus += (" " + q.ChosenAnswer);
 		}
+		
+//		URLExtractor.extractURL(corpus);
+
+		System.out.println(corpus);
 	}
 
 }
