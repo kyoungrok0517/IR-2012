cd2c07ec211c4cd4736c348ad9e6b04706dda502
Kyoung Rok Jang <astray0924@gmail.com>
2012-05-26 00:23:55 +0900

    sdf

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
 
-import com.joogle.model.TermWithFrequency;
 import com.joogle.model.YahooQuestion;
 import com.joogle.utility.TermRankingFunction;
 import com.joogle.utility.YahooAnswerHelper;
@@ -14,36 +13,19 @@ public class Main {
 		List<YahooQuestion> questions = YahooAnswerHelper
 				.searchQuestions("prime factor");
 		
-		// the collection is mega-document
-		// composed of (retrieved questions + chosen answers)  
-		String collection = "";
-		
-		for (YahooQuestion q : questions) {
-			collection += " " + q.Content;
-			collection += " " + q.ChosenAnswer;			
-		}
-		
-		// the relevant documents are chosen answers (treated as individual document)
-		List<String> all_documents = new ArrayList<String>();
-		List<String> prf_documents = new ArrayList<String>(); 
+		// build the collection & PRF documents
+		// Collection: retrieved 
+		List<String> collection = new ArrayList<String>();
+		List<String> prf_docs = new ArrayList<String>();
 		for (YahooQuestion q : questions) {
 			String question = q.Content;
 			String answer = q.ChosenAnswer;
-			all_documents.add(question);
-			all_documents.add(answer);
-			prf_documents.add(answer);
+			collection.add(question);
+			collection.add(answer);
+			prf_docs.add(answer);
 		}
-		
-//		for (TermWithFrequency twf : TermRankingFunction.getTf(collection)) {
-//			System.out.println(twf);
-//		}
-		
-		for (TermWithFrequency twf : TermRankingFunction.getTf(collection)) {
-			int df = TermRankingFunction.getDf(prf_documents, twf.term);
-			
-			System.out.println("The df of " + twf.term.trim() + " is: " + df);
-		}
-	}
 
+		
+	}
 
 }
--- src/com/joogle/utility/TermRankingFunction.java
+++ src/com/joogle/utility/TermRankingFunction.java
@@ -1,59 +1,22 @@
 package com.joogle.utility;
 
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-
-import com.joogle.model.TermWithFrequency;
+import java.util.StringTokenizer;
 
 public class TermRankingFunction {
-
-	public static List<TermWithFrequency> getTf(String document) {
-		// preprocess the document
-		String pre_document = preprocess(document);
-		
-		Map<String, Integer> term_freq_map = new HashMap<String, Integer>();
-
-		// first we count the freq. of terms
-		for (String term : Tokenizer.tokenize(pre_document)) {
-			if (!term_freq_map.containsKey(term)) {
-				term_freq_map.put(term, 1);
-			} else {
-				int new_freq = (term_freq_map.get(term) + 1);
-				term_freq_map.put(term, new_freq);
-			}
-		}
-
-		// we save the terms with their frequency for easy handling
-		List<TermWithFrequency> term_freq_list = new ArrayList<TermWithFrequency>();
-		for (String term : term_freq_map.keySet()) {
-			TermWithFrequency twf = new TermWithFrequency(term,
-					term_freq_map.get(term));
-			term_freq_list.add(twf);
-		}
-
-		// Sort the terms by their frequency
-		Collections.sort(term_freq_list);
-
-		return term_freq_list;
-	}
 	
-	public static int getDf(List<String> documents, String term) {
-		int df = 0;
-		
-		for (String doc : documents) {
-			if (doc.contains(term)) {
-				df++;
-			}
-		}
-		
-		return df;
-	}
 
-	// TODO: 각종 전처리 과정 구현 필요
-	private static String preprocess(String text) {
-		return text;
+	public static List<String> tokenize(String text) {
+		// TODO: 여기서 stopword 제거, 소문자화, stemming 등을 모두 처리
+		StringTokenizer tokenizer = new StringTokenizer(text,
+				" <>()_-.?[]{}\"\'~!@#$%^&*,/");
+
+		List<String> tokens = new ArrayList<String>();
+		while (tokenizer.hasMoreTokens()) {
+			tokens.add(tokenizer.nextToken());
+		}
+
+		return tokens;
 	}
 }
